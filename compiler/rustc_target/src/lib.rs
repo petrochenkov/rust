dd9692c6778d37ad01e1de5ecb9fcb8054557fe1
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2021-02-27 16:30:13 +0300

    [WIP] Disable default static linking and CRT fallback for musl targets on musl hosts

--- compiler/rustc_target/src/lib.rs
+++ compiler/rustc_target/src/lib.rs
@@ -30,3 +30,15 @@ pub mod spec;
 /// This is a hack to allow using the `HashStable_Generic` derive macro
 /// instead of implementing everything in librustc_middle.
 pub trait HashStableContext {}
+
+pub fn host_triple() -> &'static str {
+    // Get the host triple out of the build environment. This ensures that our
+    // idea of the host triple is the same as for the set of libraries we've
+    // actually built.  We can't just take LLVM's host triple because they
+    // normalize all ix86 architectures to i386.
+    //
+    // Instead of grabbing the host triple (for the current host), we grab (at
+    // compile time) the target triple that this rustc is built with and
+    // calling that (at runtime) the host triple.
+    (option_env!("CFG_COMPILER_HOST_TRIPLE")).expect("CFG_COMPILER_HOST_TRIPLE")
+}
--- compiler/rustc_target/src/spec/linux_musl_base.rs
+++ compiler/rustc_target/src/spec/linux_musl_base.rs
@@ -5,12 +5,14 @@ pub fn opts() -> TargetOptions {
     let mut base = super::linux_base::opts();
 
     base.env = "musl".to_string();
-    base.pre_link_objects_fallback = crt_objects::pre_musl_fallback();
-    base.post_link_objects_fallback = crt_objects::post_musl_fallback();
-    base.crt_objects_fallback = Some(CrtObjectsFallback::Musl);
+    if !crate::host_triple().contains("musl") {
+        base.pre_link_objects_fallback = crt_objects::pre_musl_fallback();
+        base.post_link_objects_fallback = crt_objects::post_musl_fallback();
+        base.crt_objects_fallback = Some(CrtObjectsFallback::Musl);
 
-    // These targets statically link libc by default
-    base.crt_static_default = true;
+        // These targets statically link libc by default
+        base.crt_static_default = true;
+    }
 
     base
 }
